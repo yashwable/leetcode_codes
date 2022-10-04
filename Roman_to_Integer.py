daaa970229ff62e907b5efbe5e8d12748d682882
Yash Wable <71828925+yashwable@users.noreply.github.com>
2022-10-04 22:56:03 +0530

    Update Roman_to_Integer.py

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -2,37 +2,36 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         ans = 0
         for i in range(len(s)):
-            a = s[i]
-            if a == 'M':
+            if s[i] == 'M':
                 if i != 0 and s[i-1] == 'C':
                     ans += 800
                 else :
                     ans += 1000
-            elif a == 'D':
+            elif s[i] == 'D':
                 if i != 0 and s[i-1] == 'C':
                     ans += 300
                 else :
                     ans += 500
-            elif a == 'C':
+            elif s[i] == 'C':
                 if i != 0 and s[i-1] == 'X':
                     ans += 80
                 else :
                     ans += 100
-            elif a == 'L':
+            elif s[i] == 'L':
                 if i != 0 and s[i-1] == 'X':
                     ans += 30
                 else :
                     ans += 50
-            elif a == 'X':
+            elif s[i] == 'X':
                 if i != 0 and s[i-1] == 'I':
                     ans += 8
                 else :
                     ans += 10
-            elif a == 'V':
+            elif s[i] == 'V':
                 if i != 0 and s[i-1] == 'I':
                     ans += 3
                 else :
                     ans += 5
-            elif a == 'I':
+            elif s[i] == 'I':
                 ans += 1
         return ans
