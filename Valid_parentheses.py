07a03654b1ccfa9094a3d04c53cc167c20cc9a78
Yash Wable <71828925+yashwable@users.noreply.github.com>
2022-10-06 22:21:01 +0530

    Update Valid_parentheses.py

--- Valid_parentheses.py
+++ Valid_parentheses.py
@@ -1,19 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         list1 = []
+        open1 = list("([{")
+        close1 = list(")]}")
+        print (open1)
         for i in s :
-            if i in "([{":
+            if i in open1:
                 list1.append(i)
-            elif (len(list1) != 0 ):
-                if i == ")" and "(" == list1[-1] :
-                    del list1[-1]
-                elif i == "]" and "[" == list1[-1] :
-                    del list1[-1]
-                elif i == "}" and "{" == list1[-1] :
-                    del list1[-1]
-                else: 
-                    return False
-            else:
+            elif len(list1) == 0:
                 return False
-        if len(list1) == 0:
+            else:
+                if list1[-1] == open1[close1.index(i)]:
+                    del list1[-1]
+                else :
+                    return False
+        if len(list1) == 0 :
             return True
+       
