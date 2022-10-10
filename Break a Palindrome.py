66af0510375ff785772fa8de3d8ced0cea871f75
Yash Wable <71828925+yashwable@users.noreply.github.com>
2022-10-10 22:20:26 +0530

    Update Break a Palindrome.py

--- Break a Palindrome.py
+++ Break a Palindrome.py
@@ -1,12 +1,15 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
-        ans = ""
-        for i in range(len(palindrome)):
-            if (ord(palindrome[i]) != 97):
-                ans += chr(ord(palindrome[i]) - 1) + palindrome[i+1:]
-                return ans
-                break
-            else :
-                ans += palindrome[i] 
-        return ""
-            
+        if len(palindrome) == 1:
+            return ""
+        else:
+            ans = ""
+            for i in range(len(palindrome)):
+                if (ord(palindrome[i]) != 97):
+                    ans += chr(ord(palindrome[i]) - 1) + palindrome[i+1:]
+                    return ans
+                    break
+                else:
+                    ans += palindrome[i]
+            else:
+                return palindrome[:-1] + chr(ord(palindrome[-1]) + 1)            
