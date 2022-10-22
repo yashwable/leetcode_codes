b80ae43a234ce33b7ecb01e354f099edc828ac97
Yash Wable <71828925+yashwable@users.noreply.github.com>
2022-10-22 20:00:54 +0530

    Update zigzag-conversion.py

--- zigzag-conversion.py
+++ zigzag-conversion.py
@@ -2,11 +2,13 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows >= len(s) or numRows == 1:
             return s 
-        ans = ""
-        n = 2*(numRows-1)
-        for i in range(numRows):
-            for j in range(len(s)):
-                if (j + i) % n == 0 or (j - i) % n == 0 :
-                    ans += s[j]
-        return ans
-            
+        ans = [""] * numRows
+        index , step = 0 , 1
+        for i in s:
+            ans[index] += i 
+            if index == numRows - 1:
+                step = -1
+            elif index == 0 :
+                step = 1
+            index += step 
+        return "".join(ans)
