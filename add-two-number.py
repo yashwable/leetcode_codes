e8802f25d9aa25446dfd8dda69de92c4b56eafee
Yash Wable <71828925+yashwable@users.noreply.github.com>
2022-10-28 15:55:48 +0530

    Update add-two-number.py

--- add-two-number.py
+++ add-two-number.py
@@ -4,40 +4,71 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:  
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         l11 = l1
-        l21 = l2
+        l21 = l2 
         p1 = ListNode()
+        p11 = p1
         first_time = True
         cap = 0
-        while l11.next != None or l21.next != None:
-            while l11.next :
+        while True:
+            while l11.next and l11.next.next :
                 l11 = l11.next
-            while l21.next :
+            while l21.next and l21.next.next :
                 l21 = l21.next
+            while p11.next:
+                p11 = p11.next
 
-            p2 = ListNode(l11.val + l21.val + cap)
-            if p2.val > 9:
-                p2.val = p2.val - 10
-                cap = 1
-            else :
-                cap = 0
-            if p1.val == 0 and first_time:
-                p1 = p2
-                first_time = False
+            if l11.next == None and l21.next == None :
+                p2= ListNode(l11.val + l21.val + cap)
+                if p2.val > 9:
+                    p2.val = p2.val - 10 
+                    cap = 1
+                else :
+                    cap = 0
+                p11.next = p2
+                return p1.next
+
+            elif l11.next == None :
+                p2= ListNode(l11.val + l21.next.val + cap)
+                if p2.val > 9:
+                    p2.val = p2.val - 10 
+                    cap = 1
+                else :
+                    cap = 0
+                p11.next = p2
+                l11.val = 0
+                l21.next = None
+
+            elif l21.next == None :
+                p2 = ListNode(l11.next.val + l21.val + cap)
+                if p2.val > 9:
+                    p2.val = p2.val - 10 
+                    cap = 1
+                else :
+                    cap = 0
+                p11.next = p2
+                l21.val = 0
+                l11.next = None
+
             else:
-                last = p1
-                while last.next :
-                    last = last.next 
-                last.next = p2
-            l12 = l1
-            l22 = l2
-            while l12.next and l12.next.next:
-                l12 = l12.next
-            l12.next = None
-            while l22.next and l22.next.next:
-                l22 = l22.next
-            l22.next = None
+                p2 = ListNode(l11.next.val + l21.next.val + cap)
+                if p2.val > 9:
+                    p2.val = p2.val - 10 
+                    cap = 1
+                else :
+                    cap = 0
+                p11.next = p2
+                l11.next = None
+                l21.next = None
             l11 = l1
-            l21 = l2
-        return p1
+            l21 = l2 
+            
+        return p1.next
+
+            
+            
+
+
+                
+            
